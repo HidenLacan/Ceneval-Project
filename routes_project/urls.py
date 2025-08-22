324e24b92b331588eed15fcaef0f0f24f236154a
Luis Perez <luisernestoperezbello@gmail.com>
2025-08-22 06:01:43 -0600

    add functionality to get mnedia files

--- routes_project/urls.py
+++ routes_project/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path("accounts/",include('accounts.urls')),
 ]
 
-# Servir archivos media en desarrollo
+# Servir archivos media en desarrollo y producción
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # En producción, también servir archivos media
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
